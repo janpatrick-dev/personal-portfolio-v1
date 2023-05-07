0669c42a1cc88346410ad90f196cab94daaab8b0
janpatrick.dev <janpatrick.dev@gmail.com>
2023-05-07 11:58:33 +0800

    removed weather app from personal projects

--- src/Components/personal/Personal.js
+++ src/Components/personal/Personal.js
@@ -7,14 +7,6 @@ function Personal() {
     <section className="section-personal" id="projects">
       <h2 className="heading-secondary u-margin-bottom-medium">Personal&nbsp;Projects</h2>
       <div className="personal">
-        <Card title="Weather App" 
-              description="A simple weather app using OpenWeather API. Built with React." 
-              skills={['React', 'SCSS', '3rd Party API']}
-              links={[
-                {url: 'https://weather-app-pat.netlify.app/', iconType: 'external'},
-                {url: 'https://github.com/janpatrick-dev/weather-app', iconType: 'github'},
-                {url: 'https://trello.com/b/8tHsPhCg/weather-app', iconType: 'trello'}
-              ]} />
         <Card title="Google Keep Clone" 
               description="A clone of the Google Keep application. Built with React." 
               skills={['React', 'MongoDB', 'Express', 'Node.js', 'SCSS']}
